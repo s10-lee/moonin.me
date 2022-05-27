5111840936f879735181829f1f831b4aa21b56e6
s10 <quicksilverinveins@gmail.com>
2022-05-28 01:30:53 +0300

    small changes

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="fixed-container-scroll">
-    <header class="p-5 border-bottom">
-      <h1>MOON <br/>IN ME</h1>
-      <img src="~assets/logo.png" width="200" height="200">
+    <header class="p-5 first">
+      <h3 class="pt-7">MOON <br/>IN ME</h3>
     </header>
 
-    <div class="container-fluid">
-      <div class="row row-no-gutters">
-        <div class="col">
-          Аудио расклад - один вопрос 10 BYN
-        </div>
-        <div class="col">
-          <img src="~assets/img/photo-01.jpeg" width="500"/>
+    <section class="row second mt-5"></section>
+
+    <div class="dark mt-5">
+      <div class="container">
+        <div class="row">
+
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
